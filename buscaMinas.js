fc997bb33448401bc01ef2d81408153dfeebcc82
Paola Jocelin Septimo Velazquez <Paola Jocelin Septimo Velazquez>
2017-04-04 12:22:53 -0500

    avances, tabla 4x4 con boton

---
+++ buscaMinas.js
@@ -0,0 +1,25 @@
+function ponNumero =
+
+
+
+
+
+
+// function resalta(elemento) {
+//   switch(elemento.style.borderColor) {
+//     case 'silver':
+//     case 'silver silver silver silver':
+//     case '#c0c0c0':
+//       elemento.style.borderColor = 'black';
+//       break;
+//     case 'black':
+//     case 'black black black black':
+//     case '#000000':
+//       elemento.style.borderColor = 'silver';
+//       break;
+//   }
+// }
+//
+// <div style="width:150px; height:60px; border:thin solid silver" onmouseover="resalta(this)" onmouseout="resalta(this)">
+//   Sección de contenidos...
+// </div>
